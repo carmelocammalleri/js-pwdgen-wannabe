a0752266d169874fb133673ea49360c6b005a24d
Melozzo <carmecamma1995@gmail.com>
2023-09-06 15:58:01 +0200

    [fix]: message const

--- js/script.js
+++ js/script.js
@@ -4,11 +4,13 @@ const name = prompt ('dicci il tuo nome');
 const lastname = prompt ('dicci il tuo cognome');
 const prefColor = prompt ('dicci il tuo colore preferito');
 const number = 23;
+const password = name + lastname + prefColor +number
 
 //2.
-const message = 'Ciao' + ' ' + name  + ' ' + 'usa' + ' ' + 'questa' + ' ' + 'password' + ':' + ' ' + name + lastname + prefColor + number;
-
-console.log(message);
+const message = `Ciao ${name} usa questa password: ${password}`
 
 //3.
 document.getElementById('output') .innerHTML = message;
+
+console.log(message);
+
