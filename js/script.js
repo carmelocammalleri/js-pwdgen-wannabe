0538bd7d3187cd45e7ff45a9dca9992a4291b246
Melozzo <carmecamma1995@gmail.com>
2023-09-06 15:15:44 +0200

    [add]: message const

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
-const name = prompt ('dicci il tuo nome')
-const lastname = prompt ('dicci il tuo cognome')
-const prefColor = prompt ('dicci il tuo colore preferito')
+
+// 1.
+const name = prompt ('dicci il tuo nome');
+const lastname = prompt ('dicci il tuo cognome');
+const prefColor = prompt ('dicci il tuo colore preferito');
+const number = 23;
+
+//2.
+const message = name  + lastname + prefColor + number;
+
+console.log(message);
+
+//3.
